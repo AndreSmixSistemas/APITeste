3cbe4b1948cc55ed410677d6726139015eb5acda
AndreSmixSistemas <51373416+AndreSmixSistemas@users.noreply.github.com>
2024-11-13 17:09:59 +0000

    Update Program.cs

--- APITeste/Program.cs
+++ APITeste/Program.cs
@@ -10,17 +10,19 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+        c.RoutePrefix = string.Empty; 
+    });
 }
 
-app.UseRouting();
+app.UseHttpsRedirection();
 
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
+app.UseAuthorization();
 
 app.MapControllers();
 
